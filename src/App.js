43f57733f26ce9468df43f2d152a41f6b6e9b087
jaydeep ganguly <65355567+jaydeepganguly@users.noreply.github.com>
2022-01-09 21:05:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-import React, { useRef, useState, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
+import React, { useRef, useEffect } from "react";
+// import * as tf from "@tensorflow/tfjs";
 
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
